46354c35f674d27894e2cf25f6dd019d4de9f0f4
David Gergiev <gutnov3643@gmail.com>
2026-06-21 22:25:22 +0300

    feat: extend config with storage, S3, and presign TTL

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,10 +9,21 @@ import (
 )
 
 type Config struct {
-	HTTP HTTPConfig
-	DB   DBConfig
-	JWT  JWTConfig
-	Log  LogConfig
+	HTTP    HTTPConfig
+	DB      DBConfig
+	JWT     JWTConfig
+	Log     LogConfig
+	Storage StorageConfig
+}
+
+type StorageConfig struct {
+	UploadDir   string `mapstructure:"upload_dir"`
+	S3Endpoint  string `mapstructure:"s3_endpoint"`
+	S3Bucket    string `mapstructure:"s3_bucket"`
+	S3AccessKey string `mapstructure:"s3_access_key"`
+	S3SecretKey string `mapstructure:"s3_secret_key"`
+	S3Region    string `mapstructure:"s3_region"`
+	PresignTTL  string `mapstructure:"presign_ttl"` // e.g. "1h"
 }
 
 type HTTPConfig struct {
@@ -42,6 +53,9 @@ func Load() (*Config, error) {
 	v.SetDefault("jwt.ttl", "24h")
 	v.SetDefault("log.level", "info")
 	v.SetDefault("log.format", "json")
+	v.SetDefault("storage.upload_dir", "./uploads")
+	v.SetDefault("storage.s3_region", "ru-central1")
+	v.SetDefault("storage.presign_ttl", "1h")
 
 	v.SetConfigName("config")
 	v.SetConfigType("yaml")
